f339615943b8b2eddac51d0cec4eccdd74f68c5d
yueyue <15584160674@163.com>
2019-05-20 15:59:08 +0800

    first commit

---
+++ sellapp/src/main.js
@@ -0,0 +1,57 @@
+import Vue from 'vue'
+import Layout from './components/layout'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+import IndexPage from './pages/index'
+import DetailPage from './pages/detail'
+import OrderListPage from './pages/orderList'
+import DetailAnaPage from './pages/detail/analysis'
+import DetailCouPage from './pages/detail/count'
+import DetailForPage from './pages/detail/forecast'
+import DetailPubPage from './pages/detail/publish'
+import Store from './store'
+Vue.use(VueRouter)
+Vue.use(VueResource)
+let router = new VueRouter({
+	mode:'history',
+	routes:[
+		{
+			path:'/',
+			component:IndexPage
+		},
+		{
+			path:'/orderList',
+			component:OrderListPage
+		},
+		{
+			path:'/detail',
+			component:DetailPage,
+			redirect:'/detail/analysis',
+			children:[
+			{
+				path:'analysis',
+				component:DetailAnaPage
+			},
+			{
+				path:'count',
+				component:DetailCouPage
+			},
+			{
+				path:'forecast',
+				component:DetailForPage
+			},
+			{
+				path:'publish',
+				component:DetailPubPage
+			}
+			]
+		}
+	]
+})
+new Vue({
+	el:'#app' ,
+	router,
+	store:Store,
+  	template: '<Layout/>',
+  	components: { Layout } 
+})
